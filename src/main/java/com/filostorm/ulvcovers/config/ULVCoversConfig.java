71ceeb7ea46d7b6aecc130fcd22e92826551da8c
shawn1368 <huaen.lim@gmail.com>
2022-06-26 11:29:18 +0800

    Minor Edit

--- src/main/java/com/filostorm/ulvcovers/config/ULVCoversConfig.java
+++ src/main/java/com/filostorm/ulvcovers/config/ULVCoversConfig.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.config.Config;
 
 @Config(modid = ULVCovers.MODID)
 public class ULVCoversConfig {
-    @Config.Comment("Recipe Type. Options: \"none\" (no generated recipes), \"easy\" (2x2 crafting), \"hard\" (3x3 crafting). Default: \"hard\"")
+    @Config.Comment("Recipe Type. Options: none (no generated recipes), easy (2x2 crafting), hard (3x3 crafting). Default: hard")
     @Config.RequiresMcRestart
     public static String recipeType = "hard";
 
